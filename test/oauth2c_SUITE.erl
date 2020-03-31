19e5b5ffbdcde7662b1ec0edcd4aa5a27da8dea9
Jakob Svenningsson <jakob.svenningsson@kivra.com>
2020-03-31 17:56:55 +0200

    Added test for recently added delate feature

--- test/oauth2c_SUITE.erl
+++ test/oauth2c_SUITE.erl
@@ -3,6 +3,7 @@
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
+-include("oauth2c.hrl").
 
 -define(AUTH_URL, <<"https://authurl.com">>).
 -define(INVALID_TOKEN_AUTH_URL, <<"https://invalidauthurl.com">>).
@@ -17,13 +18,15 @@
 groups() -> [].
 
 all() -> [ retrieve_access_token
-         , retrieve_cached_access_token
-         , retrieve_cached_expired_access_token
-         , retrieve_access_token_burst
-         , retrieve_access_token_burst_with_expire
          , fetch_access_token_on_request
          , fetch_access_token_on_request
          , fetch_new_token_on_401
+         , retrieve_cached_access_token
+         , retrieve_cached_expired_access_token
+         , retrieve_cached_token_burst
+         , retrieve_cached_token_burst_with_expire
+         , retrieve_cached_token_on_401
+         , retrieve_cached_token_on_401_burst
          ].
 
 init_per_suite(Config) ->
@@ -34,6 +37,12 @@ end_per_suite(Config) ->
   exit(Pid, shutdown),
   ok.
 
+init_per_testcase(retrieve_cached_token_on_401_burst, Config) ->
+  mock_http_request_401_burst(),
+  Config;
+init_per_testcase(retrieve_cached_token_on_401, Config) ->
+  mock_http_request_401(),
+  Config;
 init_per_testcase(_TestCase, Config) ->
   mock_http_requests(),
   Config.
@@ -88,7 +97,7 @@ retrieve_cached_expired_access_token(_Config) ->
                                   ?AUTH_URL, '_', '_', '_', '_'
                                 ])).
 
-retrieve_access_token_burst(_Config) ->
+retrieve_cached_token_burst(_Config) ->
   % The cache server should be able to handle multiple concurrent requests
   % and only perform a single token request.
   N = 1000,
@@ -108,7 +117,7 @@ retrieve_access_token_burst(_Config) ->
                                   ?AUTH_URL, '_', '_', '_', '_'
                                 ])).
 
-retrieve_access_token_burst_with_expire(_Config) ->
+retrieve_cached_token_burst_with_expire(_Config) ->
   N = 1000,
   Fun = fun() ->
           oauth2c:retrieve_access_token(?CLIENT_CREDENTIALS_GRANT,
@@ -129,6 +138,47 @@ retrieve_access_token_burst_with_expire(_Config) ->
                                 [ post, percent,
                                   ?AUTH_URL, '_', '_', '_', '_'
                                 ])).
+
+retrieve_cached_token_on_401(_Config) ->
+  {ok, _, Client} = oauth2c:retrieve_access_token(?CLIENT_CREDENTIALS_GRANT,
+                                                  ?AUTH_URL,
+                                                  <<"ID">>,
+                                                  <<"SECRET">>,
+                                                  undefined,
+                                                  [{enable_cache, true}]),
+  Response = oauth2c:request(get, json,
+    ?REQUEST_URL, [], [], [], [enable_cache], Client),
+  ?assert(2 =:= meck:num_calls(restc, request,
+                              [ post, percent,
+                                ?AUTH_URL, '_', '_', '_', '_'
+                              ])),
+  ?assertMatch({{ok, 200, _, _}, _}, Response),
+  {_, Client1} = Response,
+  ?assert(Client#client.expiry_time < Client1#client.expiry_time).
+
+retrieve_cached_token_on_401_burst(_Config) ->
+  {ok, _, Client} = oauth2c:retrieve_access_token(?CLIENT_CREDENTIALS_GRANT,
+                                                  ?AUTH_URL,
+                                                  <<"ID">>,
+                                                  <<"SECRET">>,
+                                                  undefined,
+                                                  [{enable_cache, true}]),
+  N = 1000,
+  Fun = fun() ->
+          Response = oauth2c:request(get, json,
+          ?REQUEST_URL, [], [], [], [enable_cache], Client),
+          ?assertMatch({{ok, 401, _, _}, Client}, Response)
+        end,
+  process_flag(trap_exit, true),
+  [spawn_link(Fun) || _ <- lists:seq(1, N)],
+  [receive {'EXIT', _, _} -> ok end || _ <- lists:seq(1, N - 1)],
+  ?assertEqual(2, meck:num_calls(restc, request,
+                            [ post, percent,
+                              ?AUTH_URL, '_', '_', '_', '_'
+                            ])).
+
+
+
 
 fetch_access_token_and_do_request(_Config) ->
   {ok, _, Client} = oauth2c:retrieve_access_token(?CLIENT_CREDENTIALS_GRANT,
@@ -184,6 +234,52 @@ mock_http_requests() ->
                     _ -> {ok, 401, [], []}
                   end
               end).
+
+mock_http_request_401() ->
+  meck:expect(restc, request,
+    [
+      {[post, percent, ?AUTH_URL, [200], '_', '_', '_'],
+        meck:loop([
+          {ok, 200, [], [{<<"access_token">>, ?VALID_TOKEN},
+                          {<<"expiry_time">>, erlang:system_time(second) + 1},
+                          {<<"token_type">>, <<"bearer">>}]},
+          {ok, 200, [], [{<<"access_token">>, ?VALID_TOKEN},
+                          {<<"expiry_time">>, erlang:system_time(second) + 10},
+                          {<<"token_type">>, <<"bearer">>}]}
+        ])
+      },
+        {[get, json, ?REQUEST_URL, '_', '_', '_', '_'],
+          meck:loop([
+            {ok, 401, [], [{<<"access_token">>, <<"invalid">>},
+                          {<<"token_type">>, <<"bearer">>}]},
+            {ok, 200, [], [{<<"access_token">>, ?VALID_TOKEN},
+                          {<<"expiry_time">>, erlang:system_time(second) + 1},
+                          {<<"token_type">>, <<"bearer">>}]}
+          ])
+        }
+    ]
+  ).
+
+mock_http_request_401_burst() ->
+  Now = erlang:system_time(second),
+  meck:expect(restc, request,
+    [
+      {[post, percent, ?AUTH_URL, [200], '_', '_', '_'],
+        meck:loop([
+          {ok, 200, [], [{<<"access_token">>, ?VALID_TOKEN},
+                          {<<"expiry_time">>, Now + 1},
+                          {<<"token_type">>, <<"bearer">>}]},
+          {ok, 200, [], [{<<"access_token">>, ?VALID_TOKEN},
+                          {<<"expiry_time">>, Now + 5},
+                          {<<"token_type">>, <<"bearer">>}]}
+        ])
+      },
+      {[get, json, ?REQUEST_URL, '_', '_', '_', '_'],
+            {ok, 401, [], [{<<"access_token">>, <<"invalid">>},
+                          {<<"token_type">>, <<"bearer">>}]}
+      }
+    ]
+  ).
 
 %_* Editor ===================================================================
 % Local Variables:
--- test/oauth2c_token_cache_SUITE.erl
+++ test/oauth2c_token_cache_SUITE.erl
@@ -3,11 +3,14 @@
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
+-include("oauth2c.hrl").
 
 
 all() -> [ get_valid_token
          , get_expired_token
          , set_and_get_token
+         , delete_token
+         , delete_token_invalid_ttl
 ].
 
 init_per_suite(Config) ->
@@ -20,42 +23,82 @@ end_per_suite(Config) ->
   ok.
 
 init_per_testcase(TestCase, Config) ->
-  ?MODULE:TestCase({init, Config}),
-  Config.
+  ?MODULE:TestCase({init, Config}).
 
 end_per_testcase(TestCase, Config) ->
-  ?MODULE:TestCase({'end', Config}),
-  Config.
+  ?MODULE:TestCase({'end', Config}).
 
-set_and_get_token({init, _Config}) -> ok;
-set_and_get_token({'end', _Config}) ->
-  oauth2c_token_cache:clear();
-set_and_get_token(_Config) ->
+get_valid_token({init, Config}) ->
+  Config;
+get_valid_token({'end', Config}) ->
+  oauth2c_token_cache:clear(),
+  Config;
+get_valid_token(_Config) ->
+  Client = #client{expiry_time = erlang:system_time(second) + 100},
   LazyToken =
-    fun() -> {ok, header, result, erlang:system_time(second) + 100} end,
+    fun() -> {ok, header, Client} end,
+  oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken),
+  ?assertMatch([{header, Client}], oauth2c_token_cache:get(?FUNCTION_NAME)).
+
+set_and_get_token({init, Config}) -> Config;
+set_and_get_token({'end', Config}) ->
+  oauth2c_token_cache:clear(),
+  Config;
+set_and_get_token(_Config) ->
+  Client = #client{expiry_time = erlang:system_time(second) + 100},
+  LazyToken =
+    fun() -> {ok, header, Client} end,
   Res1 = oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken),
   Res2 = oauth2c_token_cache:get(?FUNCTION_NAME),
   [
-    ?assertMatch({ok, header, result}, Res1),
-    ?assertMatch([{header, result}], Res2)
+    ?assertMatch({ok, header, Client}, Res1),
+    ?assertMatch([{header, Client}], Res2)
   ].
 
-get_valid_token({init, _Config}) ->
-  LazyToken =
-    fun() -> {ok, header, result, erlang:system_time(second) + 100} end,
-  oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken);
-get_valid_token({'end', _Config}) ->
-  oauth2c_token_cache:clear();
-get_valid_token(_Config) ->
-  Res = oauth2c_token_cache:get(?FUNCTION_NAME),
-  ?assertMatch([{header, result}], Res).
 
-get_expired_token({init, _Config}) ->
-  LazyToken =
-    fun() -> {ok, header, result, erlang:system_time(second) - 100} end,
-  oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken);
-get_expired_token({'end', _Config}) ->
-  oauth2c_token_cache:clear();
+get_expired_token({init, Config}) -> Config;
+get_expired_token({'end', Config}) ->
+  oauth2c_token_cache:clear(),
+  Config;
 get_expired_token(_Config) ->
+  Client = #client{expiry_time = erlang:system_time(second) - 100},
+  LazyToken =
+    fun() -> {ok, header, Client} end,
+  oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken),
   Res = oauth2c_token_cache:get(?FUNCTION_NAME),
   ?assertMatch([], Res).
+
+delete_token({init, Config}) -> Config;
+delete_token({'end', Config}) ->
+  oauth2c_token_cache:clear(),
+  Config;
+delete_token(_Config) ->
+  TTL = erlang:system_time(second) + 100,
+  Client = #client{expiry_time = TTL},
+  LazyToken =
+    fun() -> {ok, header, Client} end,
+  Res1 = oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken),
+  oauth2c_token_cache:delete_token(?FUNCTION_NAME, TTL),
+  Res2 = oauth2c_token_cache:get(?FUNCTION_NAME),
+  [
+    ?assertMatch({ok, header, Client}, Res1),
+    ?assertMatch([], Res2)
+  ].
+
+delete_token_invalid_ttl({init, Config}) -> Config;
+delete_token_invalid_ttl({'end', Config}) ->
+  oauth2c_token_cache:clear(),
+  Config;
+delete_token_invalid_ttl(_Config) ->
+  TTL = erlang:system_time(second) + 100,
+  Client = #client{expiry_time = TTL},
+  LazyToken =
+    fun() -> {ok, header, Client} end,
+  Res1 = oauth2c_token_cache:set_and_get(?FUNCTION_NAME, LazyToken),
+  oauth2c_token_cache:delete_token(?FUNCTION_NAME, TTL + 1),
+  Res2 = oauth2c_token_cache:get(?FUNCTION_NAME),
+  [
+    ?assertMatch({ok, header, Client}, Res1),
+    ?assertMatch([{header, Client}], Res2)
+  ].
+
